7ba2c7db978abcf5d0f45efb658f7bcbd3b295af
Jatin <140497406+jatinriverlane@users.noreply.github.com>
2024-04-04 12:25:39 +0000

    Added autogenerated graphql queries

--- aqueductcore/frontend/src/types/graphql/__GENERATED__/gql.ts
+++ aqueductcore/frontend/src/types/graphql/__GENERATED__/gql.ts
@@ -2,16 +2,6 @@
 import * as types from './graphql';
 import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 
-/**
- * Map of all GraphQL operations in the project.
- *
- * This map has several performance disadvantages:
- * 1. It is not tree-shakeable, so it will include all operations in the project.
- * 2. It is not minifiable, so the string of a GraphQL query will be multiple times inside the bundle.
- * 3. It does not support dead code elimination, so it will add unused operations.
- *
- * Therefore it is highly recommended to use the babel or swc plugin for production.
- */
 const documents = {
     "\n  mutation addTagToExperiment($experimentId: UUID!, $tag: String!) {\n    addTagToExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n": types.AddTagToExperimentDocument,
     "\n  mutation removeTagFromExperiment($experimentId: UUID!, $tag: String!) {\n    removeTagFromExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n": types.RemoveTagFromExperimentDocument,
@@ -22,37 +12,15 @@ const documents = {
     "\n  query getExperimentFilesById($experimentIdentifier: ExperimentIdentifierInput!) {\n    experiment(experimentIdentifier: $experimentIdentifier) {\n      files {\n        name\n        path\n        modifiedAt\n      }\n    }\n  }\n": types.GetExperimentFilesByIdDocument,
 };
 
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- *
- *
- * @example
- * ```ts
- * const query = graphql(`query GetUser($id: ID!) { user(id: $id) { name } }`);
- * ```
- *
- * The query argument is unknown!
- * Please regenerate the types.
- */
-export function graphql(source: string): unknown;
-
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
 export function graphql(source: "\n  mutation addTagToExperiment($experimentId: UUID!, $tag: String!) {\n    addTagToExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n"): (typeof documents)["\n  mutation addTagToExperiment($experimentId: UUID!, $tag: String!) {\n    addTagToExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n"];
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
 export function graphql(source: "\n  mutation removeTagFromExperiment($experimentId: UUID!, $tag: String!) {\n    removeTagFromExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n"): (typeof documents)["\n  mutation removeTagFromExperiment($experimentId: UUID!, $tag: String!) {\n    removeTagFromExperiment(experimentTagInput: { experimentId: $experimentId, tag: $tag }) {\n      id\n      tags\n    }\n  }\n"];
-/**
- * The graphql function is used to parse GraphQL queries into a document that can be used by GraphQL clients.
- */
 export function graphql(source: "\n  mutation updateExperimentName(\n    $experimentId: UUID!\n    $experimentUpdateInput: ExperimentUpdateInput!\n  ) {\n    updateExperiment(experimentId: $experimentId, experimentUpdateInput: $experimentUpdateInput) {\n      id\n      title\n      description\n      alias\n    }\n  }\n"): (typeof documents)["\n  mutation updateExperimentName(\n    $experimentId: UUID!\n    $experimentUpdateInput: ExperimentUpdateInput!\n  ) {\n    updateExperiment(experimentId: $experimentId, experimentUpdateInput: $experimentUpdateInput) {\n      id\n      title\n      description\n      alias\n    }\n  }\n"];
 export function graphql(source: "\n  query getAllExperiments($offset: Int!, $limit: Int!, $filters: ExperimentFiltersInput) {\n    experiments(offset: $offset, limit: $limit, filters: $filters) {\n      experimentsData {\n        id\n        alias\n        title\n        description\n        tags\n        createdAt\n        createdBy\n      }\n      totalExperimentsCount\n    }\n  }\n"): (typeof documents)["\n  query getAllExperiments($offset: Int!, $limit: Int!, $filters: ExperimentFiltersInput) {\n    experiments(offset: $offset, limit: $limit, filters: $filters) {\n      experimentsData {\n        id\n        alias\n        title\n        description\n        tags\n        createdAt\n        createdBy\n      }\n      totalExperimentsCount\n    }\n  }\n"];
 export function graphql(source: "\n  query getAllTags {\n    tags {\n      tagsData\n    }\n  }\n"): (typeof documents)["\n  query getAllTags {\n    tags {\n      tagsData\n    }\n  }\n"];
 export function graphql(source: "\n  query getExperimentById($experimentIdentifier: ExperimentIdentifierInput!) {\n    experiment(experimentIdentifier: $experimentIdentifier) {\n      id\n      title\n      description\n      tags\n      alias\n      createdAt\n      createdBy\n      files {\n        name\n        path\n        modifiedAt\n      }\n    }\n  }\n"): (typeof documents)["\n  query getExperimentById($experimentIdentifier: ExperimentIdentifierInput!) {\n    experiment(experimentIdentifier: $experimentIdentifier) {\n      id\n      title\n      description\n      tags\n      alias\n      createdAt\n      createdBy\n      files {\n        name\n        path\n        modifiedAt\n      }\n    }\n  }\n"];
 export function graphql(source: "\n  query getExperimentFilesById($experimentIdentifier: ExperimentIdentifierInput!) {\n    experiment(experimentIdentifier: $experimentIdentifier) {\n      files {\n        name\n        path\n        modifiedAt\n      }\n    }\n  }\n"): (typeof documents)["\n  query getExperimentFilesById($experimentIdentifier: ExperimentIdentifierInput!) {\n    experiment(experimentIdentifier: $experimentIdentifier) {\n      files {\n        name\n        path\n        modifiedAt\n      }\n    }\n  }\n"];
 
+export function graphql(source: string): unknown;
 export function graphql(source: string) {
   return (documents as any)[source] ?? {};
 }
