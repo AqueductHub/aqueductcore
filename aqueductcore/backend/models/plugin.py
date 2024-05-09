4698c72036a5b5d734bc9018485024abff33a80d
Stanislav Protasov <stanislav.protasov@riverlane.com>
2024-05-09 09:37:53 +0100

    Revert "feat(plugins): #91 adding venvs naturally for each plugin"
    This reverts commit 2d67d8b33f4fc3b9d77e48de6e3f6a762092471a.

--- aqueductcore/backend/models/plugin.py
+++ aqueductcore/backend/models/plugin.py
@@ -120,7 +120,6 @@ class PluginFunction(BaseModel):
             self,
             plugin: Plugin,
             params: dict,
-            python: str | Path | None = None,
             timeout: int=60) -> PluginExecutionResult:
         """Passes parameters to the function code and awaits
         execution results
@@ -143,14 +142,8 @@ class PluginFunction(BaseModel):
         cwd = Path.home()
         if plugin.manifest_file:
             cwd = Path(plugin.manifest_file).parent
-
-        print(python)
-        rich_script = self.script
-        print(rich_script)
-        if python:
-            rich_script = rich_script.replace("$python ", str(python) + " ")
         with subprocess.Popen(
-            rich_script,
+            self.script,
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
@@ -210,7 +203,6 @@ class Plugin(BaseModel):
     manifest_file: Optional[str] = None
     aqueduct_key: Optional[str] = None
     params: Optional[Dict[str, Any]] = None
-    folder: Path = Path.home()
 
     @classmethod
     def from_folder(cls, path: Path) -> Plugin:
@@ -231,7 +223,6 @@ class Plugin(BaseModel):
             # TODO: somehow generate and pass it here
             plugin.aqueduct_key = ""
             plugin.validate_object()
-            plugin.folder = path
             return plugin
 
     def get_function(self, name: str) -> PluginFunction:
