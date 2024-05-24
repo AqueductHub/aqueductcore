2540447be4c09b34a296b9b86ce744d969f273b4
Jatin <140497406+jatinriverlane@users.noreply.github.com>
2024-05-24 14:03:49 +0000

    updated query

--- aqueductcore/frontend/src/API/graphql/queries/getAllPlugins.tsx
+++ aqueductcore/frontend/src/API/graphql/queries/getAllPlugins.tsx
@@ -2,16 +2,30 @@ import { gql, QueryHookOptions, useQuery } from "@apollo/client";
 import { GET_ALL_PLUGINS_TYPE } from "types/globalTypes";
 
 export const GET_ALL_PLUGINS = gql`
-  query getAllTags {
-    tags {
-      tagsData
+  query getAllPlugins {
+    plugins {
+      authors
+      description
+      functions {
+        description
+        experimentVariableName
+        name
+        parameters {
+          dataType
+          defaultValue
+          description
+          displayName
+          name
+          options
+        }
+      }
     }
   }
 `;
 
-export function useGetAllTags(options?: QueryHookOptions) {
-  const experiments = useQuery<GET_ALL_PLUGINS_TYPE>(GET_ALL_PLUGINS, {
+export function useGetAllPlugins(options?: QueryHookOptions) {
+  const plugins = useQuery<GET_ALL_PLUGINS_TYPE>(GET_ALL_PLUGINS, {
     ...options
   });
-  return experiments;
+  return plugins;
 }
