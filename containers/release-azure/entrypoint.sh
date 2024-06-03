e6816de579d0281326c5f1fbacfb125b28e82b74
Seyed Amir Alavi <amir.alavi@riverlane.com>
2024-06-03 12:49:27 +0000

    fix(docker): Add step to save env variables for later SSH access.

--- containers/release-azure/entrypoint.sh
+++ containers/release-azure/entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 set -e
 service ssh start
+
+# Save all environment variables to /etc/environment for later use via SSH
+env | grep _ >> /etc/environment
+
 exec gunicorn -w 1 -k uvicorn.workers.UvicornWorker aqueductcore.backend.main:app --bind 0.0.0.0:8000 --timeout 60
