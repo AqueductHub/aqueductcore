e269199e2d22f841db37f32f476508d1c1068709
Seyed Amir Alavi <amir.alavi@riverlane.com>
2024-06-26 16:37:09 +0100

    fix(docker): Set the number of workers for release docker images to 4. (#170)
    set the number of workers for release docker images to 4.

--- containers/release-azure/entrypoint.sh
+++ containers/release-azure/entrypoint.sh
@@ -5,4 +5,4 @@ service ssh start
 # Save all environment variables to /etc/environment for later use via SSH
 env | grep _ >> /etc/environment
 
-exec gunicorn -w 1 -k uvicorn.workers.UvicornWorker aqueductcore.backend.main:app --bind 0.0.0.0:8000 --timeout 60
+exec gunicorn -w 4 -k uvicorn.workers.UvicornWorker aqueductcore.backend.main:app --bind 0.0.0.0:8000 --timeout 60
