37b73a3919afc3c0e22171416983c1480ce35534
safeamiiir <amirreza.safehian@riverlane.com>
2024-07-01 18:03:15 +0000

    feat(upload): add upload button functionality

--- aqueductcore/frontend/src/components/organisms/Attachments/index.tsx
+++ aqueductcore/frontend/src/components/organisms/Attachments/index.tsx
@@ -1,12 +1,15 @@
-import { Grid, Typography, styled } from "@mui/material";
 // import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
-// import FileDownloadOutlinedIcon from "@mui/icons-material/FileDownloadOutlined";
-// import UploadFileOutlinedIcon from "@mui/icons-material/UploadFileOutlined";
-// import { Divider } from "@mui/material";
-import { useContext } from "react";
+import UploadFileOutlinedIcon from "@mui/icons-material/UploadFileOutlined";
+import { Grid, Typography, styled } from "@mui/material";
+import { ChangeEvent, useContext } from "react";
+import toast from "react-hot-toast";
 
+import { BorderedButtonWithIcon } from "components/atoms/sharedStyledComponents/BorderedButtonWithIcon";
+import { VisuallyHiddenInput } from "components/atoms/sharedStyledComponents/VisuallyHiddenInput";
 import { FileSelectStateContext } from "context/FileSelectProvider";
 import { ExperimentFileType } from "types/globalTypes";
+import { client } from "API/apolloClientConfig";
+import { AQD_FILE_URI } from "constants/api";
 import Explorer from "./Explorer";
 import Viewer from "./Viewer";
 
@@ -14,17 +17,6 @@ const SectionTitle = styled(Typography)`
 font-size: 1.15rem;
 margin-top: ${(props) => `${props.theme.spacing(1.5)}`};
 `;
-
-// const BorderedButtonWithIcon = styled(Button)`
-//   border-color: ${(props) => props.theme.palette.neutral.main};
-//   color: ${(props) =>
-//     props.theme.palette.mode === "dark"
-//       ? props.theme.palette.common.white
-//       : props.theme.palette.common.black};
-//   text-transform: none;
-//   padding-left: ${(props) => `${props.theme.spacing}`};
-//   padding-right: ${(props) => `${props.theme.spacing}`};
-// `;
 
 interface AttachmentProps {
   experimentUuid: ExperimentFileType[];
@@ -34,35 +26,77 @@ interface AttachmentProps {
 function Attachments({ experimentUuid, experimentFiles }: AttachmentProps) {
   const { selectedFile, setSelectedFile } = useContext(FileSelectStateContext)
 
+  function handleExperimentFileUpload(e: ChangeEvent<HTMLInputElement>) {
+    if (e.target.files) {
+      const file = e.target?.files[0]
+      const fileURL = file ? `${AQD_FILE_URI}/api/files/${experimentUuid}` : "";
+
+      const data = new FormData();
+      data.append('file', file);
+
+      fetch(fileURL, {
+        method: 'POST',
+        headers: {
+          file_name: file.name
+        },
+        body: data
+      })
+        .then((response) => {
+          const status = response.status
+          const statusText = response.statusText
+          // SUCCESS
+          if (status === 200) {
+            response.json().then(async (data) => {
+              await client.refetchQueries({
+                include: "active",
+              });
+              // ! file.name is fronm client! might cause issues,
+              // TODO: it should be in the response.
+              setSelectedFile(file.name)
+              toast.success(data.result ?? "File uploaded successfully!", {
+                id: "upload_success",
+              });
+            })
+            // Response failure message
+          } else {
+            response.json().then((error) => {
+              toast.error(error.detail ?? statusText, {
+                id: "upload_failed",
+              });
+            })
+              // No failure message
+              .catch(() => {
+                toast.error(statusText, {
+                  id: "upload_catch",
+                });
+              });
+          }
+        })
+    }
+  }
+
   return (
     <>
       <SectionTitle sx={{ mt: 3 }}>Attachment</SectionTitle>
-      {/* // TODO:  will be uncommented when functionality is back*/}
-      {/* <Grid container spacing={1} sx={{ mt: 0.5 }}>
+      <Grid container spacing={1} sx={{ mt: 0.5 }}>
         <Grid item>
           <BorderedButtonWithIcon
+            // @ts-expect-error is not assignable to type
+            component="label"
+            role={undefined}
             variant="outlined"
             size="small"
             color="neutral"
             startIcon={<UploadFileOutlinedIcon />}
           >
             File upload
+            <VisuallyHiddenInput type="file" onChange={handleExperimentFileUpload} />
           </BorderedButtonWithIcon>
         </Grid>
-        <Grid item>
+        {/* <Grid item>
           <Divider orientation="vertical" />
-        </Grid>
-        <Grid item>
-          <BorderedButtonWithIcon
-            variant="outlined"
-            size="small"
-            color="neutral"
-            startIcon={<FileDownloadOutlinedIcon />}
-          >
-            Download
-          </BorderedButtonWithIcon>
-        </Grid>
-        <Grid item>
+        </Grid> */}
+        {/* <Grid item>
           <BorderedButtonWithIcon
             variant="outlined"
             size="small"
@@ -71,8 +105,8 @@ function Attachments({ experimentUuid, experimentFiles }: AttachmentProps) {
           >
             Delete
           </BorderedButtonWithIcon>
-        </Grid>
-      </Grid> */}
+        </Grid> */}
+      </Grid>
       <Grid container spacing={2} sx={{ mt: 0 }}>
         <Grid item xs={12} lg={6}>
           <Explorer
