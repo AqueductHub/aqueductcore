901fca92236a83fcf243c9c28f815176dfb12fec
Stanislav Protasov <stanislav.protasov@riverlane.com>
2024-07-17 16:56:42 +0000

    Verbose execution

--- scripts/add_extensions_from.sh
+++ scripts/add_extensions_from.sh
@@ -1,13 +1,24 @@
-set -x
 cwd=$(pwd)
-mytmpdir=${TMPDIR:-/tmp}
-where=$mytmpdir/external-extensions/
 branch=${2:-main}
+mytmpdir=${TMPDIR:-/tmp}
+
+echo "======== Creating a temp dir ========"
+where=$mytmpdir/external-extensions/
+echo "TMP: $where"
 rm -rf $where
 mkdir -p $where
+
+echo "=========== Cloning ================="
 echo "$1 -> $where:"
 git clone $1 -- $where
 cd $where
+
+echo "====== Switching a branch ==========="
+echo "-> $branch"
 git checkout $branch
+
+echo "=========== Copying ================="
 find $where/ -mindepth 1 -maxdepth 1 -type d -not \( -name ".git*" \) -exec cp -r "{}" $cwd/external/extensions/ \;
+
+echo "=========== Cleaning ================"
 rm -rf $where
