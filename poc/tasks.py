2342e96c03b2df4cb3b3231b40ecfb0fd464aab7
Stanislav Protasov <stanislav.protasov@riverlane.com>
2024-07-23 10:35:08 +0100

    adding dev containers

--- poc/tasks.py
+++ poc/tasks.py
@@ -9,6 +9,7 @@ def run_executable(self, shell_script, **kwargs):
     myenv = os.environ.copy()
     myenv.update(kwargs)
     print("ARGS: ", myenv)
+    self.update_state(state='CUSTOM_RIVERLANE_STATE_1', meta={'done': 0, 'total': 1})
     with subprocess.Popen(
             shell_script,
             shell=True,
@@ -18,5 +19,5 @@ def run_executable(self, shell_script, **kwargs):
         ) as proc:
             out, err = proc.communicate(timeout=60)
             code = proc.returncode
-    self.update_state(state='CUSTOM_RIVERLANE_STATE', meta={'done': 1, 'total': 1})
+    self.update_state(state='CUSTOM_RIVERLANE_STATE_2', meta={'done': 1, 'total': 1})
     return code, out, err
