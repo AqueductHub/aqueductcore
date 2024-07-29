e5cb9c2459bed4f3aa8cc361647f100daa212aae
Stanislav Protasov <stanislav.protasov@riverlane.com>
2024-07-29 09:55:51 +0100

    add

--- aqueductcore/backend/services/task_executor.py
+++ aqueductcore/backend/services/task_executor.py
@@ -11,6 +11,7 @@ from celery.result import AsyncResult
 app = Celery('tasks', broker='pyamqp://guest@rabbit-with-celery-container//', backend='rpc://')
 
 @app.task(bind=True)
+<<<<<<< HEAD
 def run_executable(
     self,
     extension_directory_name: str,
@@ -40,6 +41,20 @@ def run_executable(
     myenv.update(kwargs)
 
     self.update_state(state='CUSTOM_RIVERLANE_STATE_1', meta={'done': 0, 'total': 1})
+=======
+def run_executable(self, extension_folder_name, shell_script, **kwargs):
+    extensions_dir = os.environ.get("EXTENSIONS_DIR", "")
+    if not extensions_dir:
+        raise FileNotFoundError("EXTENSIONS_DIR environment variable should be set.")
+    workdir = Path(extensions_dir) / extension_folder_name
+
+    myenv = os.environ.copy()
+    myenv.update(kwargs)
+    print("ARGS: ", myenv)
+    time.sleep(2)
+    self.update_state(state='CUSTOM_RIVERLANE_STATE_1', meta={'done': 0, 'total': 1})
+    time.sleep(2)
+>>>>>>> 10417b5 (adding task execution feature)
     with subprocess.Popen(
             shell_script,
             shell=True,
@@ -48,6 +63,7 @@ def run_executable(
             env=myenv,
             cwd=workdir,
         ) as proc:
+<<<<<<< HEAD
             out, err = proc.communicate(timeout=None)
             code = proc.returncode
     self.update_state(state='CUSTOM_RIVERLANE_STATE_2', meta={'done': 1, 'total': 1})
@@ -82,3 +98,10 @@ def execute_non_blocking(
         **kwargs
     )
     return job
+=======
+            out, err = proc.communicate(timeout=60)
+            code = proc.returncode
+    self.update_state(state='CUSTOM_RIVERLANE_STATE_2', meta={'done': 1, 'total': 1})
+    time.sleep(2)
+    return code, out, err
+>>>>>>> 10417b5 (adding task execution feature)
