72caf20f8a47e132250467f652c8c3d78dca009c
Seyed Amir Alavi <amir.alavi@riverlane.com>
2024-08-06 17:12:35 +0000

    update reference release docker compose.

--- scripts/release/install.sh
+++ scripts/release/install.sh
@@ -11,7 +11,4 @@ export EXTENSIONS_DIR=$HOME/external/extensions
 mkdir -p $EXPERIMENTS_DIR
 mkdir -p $EXTENSIONS_DIR
 
-# load Aqueduct docker image
-docker load -i $SCRIPT_DIR/aqueductcore.tar
-
 docker compose -f $SCRIPT_DIR/docker-compose.yaml up -d
