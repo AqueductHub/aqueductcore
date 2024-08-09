a95b04fcd9a7c51e539c07d6f63e8384819f6a7c
safeamiiir <amirreza.safehian@riverlane.com>
2024-08-09 15:39:59 +0000

    feat(table): Add job table component

--- aqueductcore/frontend/src/components/organisms/JobsListTable/index.tsx
+++ aqueductcore/frontend/src/components/organisms/JobsListTable/index.tsx
@@ -3,50 +3,115 @@ import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
-import { Paper } from "@mui/material";
-import JobExtensionActionName from "components/molecules/JobListTableCells/JobExtensionActionName";
-import JobExtensionStatus from "components/molecules/JobListTableCells/JobExtensionStatus";
-import JobExperimentName from "components/molecules/JobListTableCells/JobExperimentName";
+import { Paper, Table, TablePagination } from "@mui/material";
+import { JobDataType, JobsListColumnsType } from "types/globalTypes";
+import { jobListRowsPerPageOptions } from "constants/constants";
+import { grey } from "@mui/material/colors";
+import { useState } from "react";
 
-function JobsListTable() {
+function JobsListTable({
+    JobRecordsColumns,
+    jobList,
+    maxHeight,
+    pageInfo,
+}: {
+    JobRecordsColumns: readonly JobsListColumnsType[];
+    jobList: JobDataType[];
+    maxHeight?: string;
+    pageInfo: {
+        page: number;
+        setPage: (number: number) => void;
+        rowsPerPage: number;
+        setRowsPerPage: (number: number) => void;
+        count: number;
+    };
+}) {
+    const [showActionId, setShowActionId] = useState("-1");
+    const { page, setPage, rowsPerPage, setRowsPerPage, count } = pageInfo;
+
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(+event.target.value);
+        setPage(0);
+    };
+
     return (
         <Paper sx={{ width: "100%", overflow: "hidden" }}>
-            <TableContainer aria-label="sticky table">
-                <TableHead>
-                    <TableRow>
-                        <TableCell>
-                            <strong>Experiment</strong>
-                        </TableCell>
-                        <TableCell>
-                            <strong>Extension</strong>
-                        </TableCell>
-                        <TableCell>
-                            <strong>Status</strong>
-                        </TableCell>
-                        <TableCell>
-                            <strong>User</strong>
-                        </TableCell>
-                        <TableCell>
-                            <strong>Submission Time</strong>
-                        </TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>
-                            <JobExperimentName />
-                        </TableCell>
-                        <TableCell>
-                            <JobExtensionActionName />
-                        </TableCell>
-                        <TableCell>
-                            <JobExtensionStatus />
-                        </TableCell>
-                        <TableCell>Jatin Lal</TableCell>
-                        <TableCell>18/07/2024 10:32</TableCell>
-                    </TableRow>
-                </TableBody>
+            <TableContainer sx={{ maxHeight }}>
+                <Table stickyHeader aria-label="sticky table">
+                    <TableHead>
+                        <TableRow>
+                            {JobRecordsColumns.map((column) => (
+                                <TableCell
+                                    key={column.id}
+                                    align={column.align}
+                                    style={{ minWidth: column.minWidth }}
+                                >
+                                    <strong>{column.label}</strong>
+                                </TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {jobList.map((row) => {
+                            return (
+                                <TableRow
+                                    hover
+                                    role="checkbox"
+                                    tabIndex={-1}
+                                    key={row.receiveTime}
+                                    onMouseEnter={() => {
+                                        setShowActionId(row.receiveTime); // set id here
+                                    }}
+                                    onMouseLeave={() => setShowActionId("-1")}
+                                    onClick={() =>
+                                        // !TODO: after TT-122 is completed
+                                        console.log("clicked", showActionId)
+                                    }
+                                    sx={{ cursor: "pointer" }}
+                                >
+                                    {JobRecordsColumns.map((column) => {
+                                        const value = row[column.id];
+                                        return (
+                                            <TableCell
+                                                key={column.id}
+                                                align={column.align}
+                                                sx={{
+                                                    textOverflow: column.ellipsis ? "ellipsis" : "initial",
+                                                    overflow: "hidden",
+                                                    whiteSpace: "nowrap",
+                                                    maxWidth: column.maxWidth,
+                                                }}
+                                            >
+                                                {column.format ? (
+                                                    column.format(value)
+                                                ) : (
+                                                    String(value)
+                                                )}
+                                            </TableCell>
+                                        );
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
             </TableContainer>
+            <TablePagination
+                sx={{
+                    borderTop: `1px solid ${grey[300]}`,
+                }}
+                rowsPerPageOptions={jobListRowsPerPageOptions}
+                component="div"
+                count={count}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+            />
         </Paper>
     );
 }
--- aqueductcore/frontend/src/pages/JobHistoryPage/index.tsx
+++ aqueductcore/frontend/src/pages/JobHistoryPage/index.tsx
@@ -3,6 +3,13 @@ import Box from "@mui/material/Box";
 
 import { drawerTopOffset, mainPadding } from "components/templates/drawerLayout";
 import JobsListTable from "components/organisms/JobsListTable";
+import { JobDataType, JobsListColumnsType } from "types/globalTypes";
+import { dateFormatter } from "helper/formatters";
+import { useState } from "react";
+import { experimentRecordsRowsPerPageOptions } from "constants/constants";
+import { ExperimentData } from "types/graphql/__GENERATED__/graphql";
+
+export const tableHeightOffset = 200;
 
 const Container = styled(Box)`
   margin: -${mainPadding}px;
@@ -16,15 +23,86 @@ const Title = styled(Box)`
   font-weight: bold;
 `;
 
+export const JobsListColumns: readonly JobsListColumnsType[] = [
+  {
+    id: "experiment",
+    label: "Experiment",
+    minWidth: 170,
+    format: (experiment) => (
+      <div>{(experiment as ExperimentData).title}+{(experiment as ExperimentData).eid}</div>
+    ),
+
+  },
+  {
+    id: "extension",
+    label: "Extension",
+    minWidth: 100,
+    format: (extension) => (
+      <div>{(extension as JobDataType['extension']).name}+{(extension as JobDataType['extension']).action}</div>
+    ),
+  },
+  {
+    id: "taskState",
+    label: "Status",
+    minWidth: 170,
+    maxWidth: 320,
+    ellipsis: true,
+  },
+  {
+    id: "username",
+    label: "User",
+    minWidth: 170,
+  },
+  {
+    id: "receiveTime",
+    label: "submission Time",
+    minWidth: 170,
+    format: (createdAt) =>
+      typeof createdAt === "string" ? dateFormatter(new Date(createdAt)) : "",
+  },
+];
+
+// TODO: this will be replaced with the Mock and some functions TT-104
+const jobListData = [
+  {
+    experiment: {
+      title: 'title_exp',
+      eid: 'eid_exp'
+    },
+    extension: {
+      name: 'name_ext',
+      action: 'action_ext'
+    },
+    taskState: 'state_1',
+    username: 'user_name',
+    receiveTime: '2023-12-25T15:43:17'
+  }
+]
+
 function JobHistoryPage() {
-    return (
-        <Container>
-            <Title>Recent Jobs</Title>
-            <Box sx={{ mt: 2 }}>
-                <JobsListTable />
-            </Box>
-        </Container>
-    );
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(experimentRecordsRowsPerPageOptions[0]);
+  const pageInfo = {
+    page,
+    setPage,
+    rowsPerPage,
+    setRowsPerPage,
+    //!TODO: add number TT-124
+    count: 100,
+  };
+  return (
+    <Container>
+      <Title>Recent Jobs</Title>
+      <Box sx={{ mt: 2 }}>
+        <JobsListTable
+          JobRecordsColumns={JobsListColumns}
+          jobList={jobListData}
+          maxHeight={`calc(100vh - ${tableHeightOffset}px)`}
+          pageInfo={pageInfo}
+        />
+      </Box>
+    </Container>
+  );
 }
 
 export default JobHistoryPage;
