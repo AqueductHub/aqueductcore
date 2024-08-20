adadd7206ac304bc0c9b5e2e30cd189e1a69cf66
safeamiiir <amirreza.safehian@riverlane.com>
2024-08-20 17:02:53 +0100

    fix(test): extension and mock

--- aqueductcore/frontend/src/__mocks__/queries/tasks/getAllTasks.tsx
+++ aqueductcore/frontend/src/__mocks__/queries/tasks/getAllTasks.tsx
@@ -1,5 +1,6 @@
 import { GET_ALL_TASKS } from "API/graphql/queries/tasks/getAllTasks";
 import { TasksDataMock } from "__mocks__/TasksDataMock";
+import { jobListRowsPerPageOptions } from "constants/constants";
 
 const request = {
     query: GET_ALL_TASKS,
@@ -8,10 +9,20 @@ const request = {
 export const getAllTasks_mock = {
     success: [
         {
-            request,
+            request: {
+                ...request,
+                variables: {
+                    offset: 0,
+                    limit: jobListRowsPerPageOptions[0],
+                    filters: {}
+                }
+            },
             result: {
                 data: {
-                    tasks: TasksDataMock
+                    tasks: {
+                        tasksData: TasksDataMock.slice(0, jobListRowsPerPageOptions[0]),
+                        totalTasksCount: TasksDataMock.slice(0, jobListRowsPerPageOptions[0]).length
+                    }
                 },
             },
             maxUsageCount: Number.POSITIVE_INFINITY,
