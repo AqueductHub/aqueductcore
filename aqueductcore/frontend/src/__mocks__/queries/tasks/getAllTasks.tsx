05b91462bd6cf57bf02a7b22d3d4ca42a4b48a15
safeamiiir <amirreza.safehian@riverlane.com>
2024-09-18 11:42:36 +0100

    feat(test): fix mock with filters

--- aqueductcore/frontend/src/__mocks__/queries/tasks/getAllTasks.tsx
+++ aqueductcore/frontend/src/__mocks__/queries/tasks/getAllTasks.tsx
@@ -1,6 +1,7 @@
 import { GET_ALL_TASKS } from "API/graphql/queries/tasks/getAllTasks";
 import { TasksDataMock } from "__mocks__/TasksDataMock";
 import { jobListRowsPerPageOptions } from "constants/constants";
+import { selected_experiment } from "../experiment/getExperimentByIdMock";
 
 const request = {
     query: GET_ALL_TASKS,
@@ -26,6 +27,30 @@ export const getAllTasks_mock = {
                 },
             },
             maxUsageCount: Number.POSITIVE_INFINITY,
-        }
+        },
+        {
+            request: {
+                ...request,
+                variables: {
+                    offset: 0,
+                    limit: jobListRowsPerPageOptions[0],
+                    filters: {
+                        experiment: {
+                            type: 'UUID',
+                            value: selected_experiment.uuid
+                        }
+                    }
+                }
+            },
+            result: {
+                data: {
+                    tasks: {
+                        tasksData: TasksDataMock.slice(0, jobListRowsPerPageOptions[0]),
+                        totalTasksCount: TasksDataMock.slice(0, jobListRowsPerPageOptions[0]).length
+                    }
+                },
+            },
+            maxUsageCount: Number.POSITIVE_INFINITY,
+        },
     ]
 };
